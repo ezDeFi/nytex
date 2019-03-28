02a0b0e1477b95d8b8de481faa4b344a0858714f
abc def <gibmireinbier@gmail.com>
2019-03-28 16:50:31 +0700

    add autofill on buy/sell if pairable with existing orders in books

--- contracts/orderbook.sol
+++ contracts/orderbook.sol
@@ -118,19 +118,151 @@ contract OrderBook is Stablio {
 		}
 
 		return id;
-	}
+    }
 
+    // buyer (active) pays buyEtherWei / buyTokenWei  etherWei for 1 tokenWei
+    // seller (passive) expects sellEtherWei / sellTokenWei etherWei for 1 tokenWei
+    // buyable = paid price >= expected price
+    // <=> buyEtherWei / buyTokenWei >= sellEtherWei / sellTokenWei
+    // <=> buyEtherWei * sellTokenWei >= buyTokenWei * sellEtherWei
+    // deal at price = sellEtherWei / sellTokenWei etherWei for 1 tokenWei
+    function buyable(
+        uint256 buyTokenWei,
+        uint256 buyEtherWei,
+        uint256 sellEtherWei,
+        uint256 sellTokenWei
+    )
+        public
+        view
+        returns(bool)
+    {
+        return buyTokenWei.mul(sellEtherWei) <= sellTokenWei.mul(buyEtherWei);
+    }
+
+    function buyComfirm(
+        address payable maker,
+        uint256 buyTokenWei,
+        uint256 buyEtherWei
+    )
+        private
+        returns(
+            uint256 restTokenWei,
+            uint256 restEtherWei
+        )
+    {
+        OrderList storage book = books[!BuyType];
+        bytes32 index = book.top;
+        Order storage order = book.orders[index];
+        // buyer bought all => restTokenWei = 0, restEtherWei = 0
+        if (order.tkn > buyTokenWei) {
+            // comfirmed eth
+            uint256 _ethTransfer = buyTokenWei.mul(order.eth) / order.tkn;
+            maker.transfer(_ethTransfer);
+            _transfer(address(this), order.maker, buyTokenWei);
+            order.eth = order.eth.sub(_ethTransfer);
+            order.tkn = order.tkn.sub(buyTokenWei);
+            book.orders[index] = order;
+            return (0, 0);
+        } else {
+        // top order filled all
+            uint256 _ethTransfer = order.eth;
+            maker.transfer(_ethTransfer);
+            _transfer(address(this), order.maker, order.tkn);
+            remove(SellType, index);
+            uint256 _restBuyTokenWei = buyTokenWei.sub(order.tkn);
+            // keep price of seller
+            uint256 _restBuyEtherWei = buyEtherWei.mul(_restBuyTokenWei) / buyTokenWei;
+            return (_restBuyTokenWei, _restBuyEtherWei);
+        }
+    }
+
+    // buyer (passive) pays buyTokenWei / buyEtherWei tokenWei for 1 etherWei
+    // seller (active) expects sellTokenWei / sellEtherWei  tokenWei for 1 etherWei
+    // sellable = paid price >= expected price
+    // <=> buyTokenWei / buyEtherWei >= sellTokenWei / sellEtherWei
+    // <=> buyTokenWei * sellEtherWei >= sellTokenWei * buyEtherWei
+    // deal at price = buyTokenWei / buyEtherWei tokenWei for 1 etherWei
+    function sellable(
+        // deposited ethm waiting to receive tokens
+        uint256 sellEtherWei,
+        uint256 sellTokenWei,
+        // deposited tokens, waiting to receive eth
+        uint256 buyTokenWei,
+        uint256 buyEtherWei
+    )
+        public
+        view
+        returns(bool)
+    {
+        return buyTokenWei.mul(sellEtherWei) >= sellTokenWei.mul(buyEtherWei);
+    }
+
+    function sellComfirm(
+        address payable maker,
+        uint256 sellEtherWei,
+        uint256 sellTokenWei
+    )
+        private
+        returns(
+            uint256 restEtherWei,
+            uint256 restTokenWei
+        )
+    {
+        OrderList storage book = books[!SellType];
+        bytes32 index = book.top;
+        Order storage order = book.orders[index];
+        // seller sold all => restEtherWei = 0, restTokenWei = 0
+        if (order.eth > sellEtherWei) {
+            // comfirmed tkn
+            uint256 _tokenTransfer = sellEtherWei.mul(order.tkn) / order.eth;
+            _transfer(address(this), maker, _tokenTransfer);
+            order.maker.transfer(sellEtherWei);
+            order.eth = order.eth.sub(sellEtherWei);
+            order.tkn = order.tkn.sub(_tokenTransfer);
+            book.orders[index] = order;
+            return (0, 0);
+        } else {
+        // top order filled all
+            uint256 _tokenTransfer = order.tkn;
+            _transfer(address(this), maker, _tokenTransfer);
+            order.maker.transfer(order.eth);
+            remove(BuyType, index);
+            uint256 _restSellEtherWei = sellEtherWei.sub(order.eth);
+            // keep price of seller
+            uint256 _restSellTokenWei = sellTokenWei.mul(_restSellEtherWei) / sellEtherWei;
+            return (_restSellEtherWei, _restSellTokenWei);
+        }
+    }
+
+	// sell eth, receive tokens, payable in case cancel
 	function sell(uint256 tokenWei, address payable maker, bytes32 index) public payable
 	returns (bytes32 orderHash) {
 		uint256 etherWei = msg.value;
 		require(tokenWei > 0);
 		require(etherWei > 0);
 
-		orderHash = insert(false, etherWei, tokenWei, maker, index);
-		emit Sell(orderHash, etherWei, tokenWei, maker);
+        // search in books[BuyType]
+        OrderList storage book = books[!SellType];
+        uint256 restEtherWei = etherWei;
+        uint256 restTokenWei = tokenWei;
+        while ((restEtherWei > 0) && (book.top != 0)) {
+            bytes32 index = book.top;
+            Order storage order = book.orders[index];
+            if (!sellable(restEtherWei, restTokenWei, order.tkn, order.eth)) {
+                break;
+            }
+            (restEtherWei, restTokenWei) = sellComfirm(maker, restEtherWei, restTokenWei);
+        }
+        if (restEtherWei == 0) {
+            // sold out, not inserted into orderbook
+            return 0;
+        }
+		orderHash = insert(false, restEtherWei, restTokenWei, maker, index);
+		emit Sell(orderHash, restEtherWei, restTokenWei, maker);
 		return orderHash;
 	}
 
+	// receive eth,  sell tokens
 	function buy(uint256 etherWei, uint256 tokenWei, address payable maker, bytes32 index) public
 	returns (bytes32 orderHash) {
 		require(tokenWei > 0);
@@ -138,6 +270,23 @@ contract OrderBook is Stablio {
 
 		// Grab the token.
 		_transfer(msg.sender, address(this), tokenWei);
+
+        // search in books[SellType]
+        OrderList storage book = books[!BuyType];
+        uint256 restTokenWei = tokenWei;
+        uint256 restEtherWei = etherWei;
+        while ((restTokenWei > 0) && (book.top != 0)) {
+            bytes32 index = book.top;
+            Order storage order = book.orders[index];
+            if (!buyable(restTokenWei, restEtherWei, order.eth, order.tkn)) {
+                break;
+            }
+            (restTokenWei, restEtherWei) = sellComfirm(maker, restTokenWei, restEtherWei);
+        }
+        if (restTokenWei == 0) {
+            // sold out, not inserted into orderbook
+            return 0;
+        }
 
 		orderHash = insert(true, etherWei, tokenWei, maker, index);
 		emit Buy(orderHash, etherWei, tokenWei, maker);
