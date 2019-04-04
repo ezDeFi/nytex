6e2c758261f023210c43ae5891e6181ced41079c
Zergity <zergity@gmail.com>
2019-04-04 12:12:44 +0700

    order and redro

--- contracts/PairEx.sol
+++ contracts/PairEx.sol
@@ -15,10 +15,10 @@ contract PairEx is OrderBook {
     {
         Order memory order;
         order.wantAmount = 1;
-        OrderList storage book = books[false];
-        book.orders[bytes32(0)] = order;  
-        book = books[true]; 
-        book.orders[bytes32(0)] = order;       
+        OrderList storage orderBook = books[false];
+        orderBook.orders[bytes32(0)] = order;  
+        orderBook = books[true]; 
+        orderBook.orders[bytes32(0)] = order;       
     }
 
     function getOrderType() 
@@ -46,47 +46,51 @@ contract PairEx is OrderBook {
         uint256 wantAmount;
         bytes32 checkpoint;
         (wantAmount, checkpoint) = abi.decode(_data, (uint256, bytes32));
-        bytes32 _newId = insert(
+        bytes32 _orderID = insert(
             orderType,
             haveAmount,
             wantAmount,
             maker,
             checkpoint
         );
-        pairing(orderType, _newId);
+        pairing(orderType, _orderID);
     }
 
     function pairing(
         bool _orderType,
-        bytes32 _newId
+        bytes32 _orderID
     )
         private
     {
-        OrderList storage book = books[_orderType];
-        Order storage _newOrder = book.orders[_newId];
-        OrderList storage oppoBook = books[!_orderType];
-        bytes32 _oppoTopId = top(!_orderType);
-        while (_oppoTopId[0] != 0) {
-            Order storage _oppoOrder = oppoBook.orders[_oppoTopId];
-            // if not pairable, return
-            if (_newOrder.haveAmount.mul(_oppoOrder.haveAmount) < _newOrder.wantAmount.mul(_oppoOrder.wantAmount)) return;
-            // token type = newOrder have
-            uint256 comfirmAmount_new = getMin(_newOrder.haveAmount, _oppoOrder.wantAmount);
-            // token type = oppoOrder have
-            uint256 comfirmAmount_oppo = _oppoOrder.haveAmount * comfirmAmount_new / _oppoOrder.wantAmount;
-            // comfirm price = _oppoOrder.haveAmount / _oppoOrder.wantAmount
-            _newOrder.wantAmount = _newOrder.wantAmount * (_newOrder.haveAmount - comfirmAmount_new) / _newOrder.haveAmount;
-            _newOrder.haveAmount = _newOrder.haveAmount.sub(comfirmAmount_new);
+        bool _redroType = !_orderType;
+        OrderList storage orderBook = books[_orderType];
+        Order storage _order = orderBook.orders[_orderID];
+        OrderList storage redroBook = books[_redroType];
+        bytes32 _redroTopId = top(_redroType);
 
-            _oppoOrder.wantAmount = _oppoOrder.wantAmount * (_oppoOrder.haveAmount - comfirmAmount_oppo) / _oppoOrder.haveAmount;
-            _oppoOrder.haveAmount = _oppoOrder.haveAmount.sub(comfirmAmount_oppo);
+        while (_redroTopId[0] != 0) {
+            Order storage _redro = redroBook.orders[_redroTopId];
+            if (_order.haveAmount.mul(_redro.haveAmount) < _order.wantAmount.mul(_redro.wantAmount)) {
+                // not pairable
+                return;
+            }
+            uint256 orderPairableAmount = getMin(_order.haveAmount, _redro.wantAmount);
+            // pairable price = _redro.haveAmount / _redro.wantAmount
+            _order.wantAmount = _order.wantAmount * (_order.haveAmount - orderPairableAmount) / _order.haveAmount;
+            _order.haveAmount = _order.haveAmount.sub(orderPairableAmount);
 
-            token[!_orderType].transfer(_newOrder.maker, comfirmAmount_oppo);
-            token[_orderType].transfer(_oppoOrder.maker, comfirmAmount_new);
-            if (_oppoOrder.haveAmount == 0 || _oppoOrder.wantAmount == 0) _remove(!_orderType, _oppoTopId);
-            _oppoTopId = top(!_orderType);
-            if (_newOrder.haveAmount == 0 || _newOrder.wantAmount == 0) {
-                _remove(_orderType, _newId);
+            uint256 redroPairableAmount = _redro.haveAmount * orderPairableAmount / _redro.wantAmount;
+            _redro.wantAmount = _redro.wantAmount * (_redro.haveAmount - redroPairableAmount) / _redro.haveAmount;
+            _redro.haveAmount = _redro.haveAmount.sub(redroPairableAmount);
+
+            token[_redroType].transfer(_order.maker, redroPairableAmount);
+            token[_orderType].transfer(_redro.maker, orderPairableAmount);
+            if (_redro.haveAmount == 0 || _redro.wantAmount == 0) {
+                _remove(_redroType, _redroTopId);
+            }
+            _redroTopId = top(_redroType);
+            if (_order.haveAmount == 0 || _order.wantAmount == 0) {
+                _remove(_orderType, _orderID);
                 return;
             }
         }
