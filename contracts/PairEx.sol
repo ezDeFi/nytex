266f35cb45239df9bcd7ba7ee5f91a3bd5b3f68f
Zergity <zergity@gmail.com>
2019-04-09 17:14:24 +0700

    naming variables

--- contracts/PairEx.sol
+++ contracts/PairEx.sol
@@ -98,7 +98,7 @@ contract PairEx is OrderBook {
     // orderToFill(SellType, 456) returns an BuyType order to fill enough selling orders to create as much as 456 StableToken.
     function orderToFill(
         bool _orderType,
-        uint256 _nusdTarget //nusd
+        uint256 _stableTokenTarget
     )
         public
         view
@@ -107,22 +107,22 @@ contract PairEx is OrderBook {
         )
     {
         OrderList storage orderBook = books[_orderType];
-        uint256 nusdSum = 0;
-        uint256 wntySum = 0;
+        uint256 totalSTB = 0;
+        uint256 totalVOL = 0;
         bytes32 cursor = top(_orderType);
-        while(cursor[0] != 0 && nusdSum < _nusdTarget) {
+        while(cursor[0] != 0 && totalSTB < _stableTokenTarget) {
             Order storage _order = orderBook.orders[cursor];
-            uint256 _nusdAmount = _orderType ? _order.haveAmount : _order.wantAmount;
-            uint256 _wntyAmount = _orderType ? _order.wantAmount : _order.haveAmount;
+            uint256 stb = _orderType ? _order.haveAmount : _order.wantAmount;
+            uint256 vol = _orderType ? _order.wantAmount : _order.haveAmount;
             // break-point
-            if (nusdSum.add(_nusdAmount) >= _nusdTarget) {
-                uint256 _nusdRest = _nusdTarget.sub(nusdSum);
-                uint256 _wntyRest = _wntyAmount.mul(_nusdRest).div(_nusdAmount);
-                wntySum = wntySum.add(_wntyRest);
-                return wntySum;
+            if (totalSTB.add(stb) >= _stableTokenTarget) {
+                uint256 remainSTB = _stableTokenTarget.sub(totalSTB);
+                uint256 remainVOL = vol.mul(remainSTB).div(stb);
+                totalVOL = totalVOL.add(remainVOL);
+                return totalVOL;
             }
-            wntySum = wntySum.add(_wntyAmount);
-            nusdSum = nusdSum.add(_nusdAmount);
+            totalVOL = totalVOL.add(vol);
+            totalSTB = totalSTB.add(stb);
             cursor = _order.next;
         }
         // orders not not enough
