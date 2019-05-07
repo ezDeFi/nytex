35db2de56d11bf5f8458a9ec976e4f21b9f2f766
abc def <gibmireinbier@gmail.com>
2019-05-07 14:45:27 +0700

    direct setup in contract constructor as optional

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,13 +2,14 @@ var PairEx = artifacts.require('./PairEx.sol');
 var StableToken = artifacts.require('./tokens/StableToken.sol');
 var VolatileToken = artifacts.require('./tokens/VolatileToken.sol');
 
+const nullAddress = '0x0000000000000000000000000000000000000000'
+
 module.exports = async function(deployer) {
-    deployer.deploy(PairEx).then(async function() {
-        await deployer.deploy(StableToken)
-        await deployer.deploy(VolatileToken)
+    deployer.deploy(PairEx, nullAddress, nullAddress).then(async function() {
+        await deployer.deploy(StableToken, nullAddress)
+        await deployer.deploy(VolatileToken, nullAddress)
         let pairExInst = await PairEx.deployed()
-        await console.log('pairExInst', pairExInst)
-        // if deployed by core(bytecode copied), ignore next line and manuell call after that
+        //await console.log('pairExInst', pairExInst)
         await pairExInst.setup(VolatileToken.address, StableToken.address)
     });
 };
