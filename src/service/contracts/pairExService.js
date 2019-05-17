7a052edcdfcbf847f88bf81b12008851767bbf9e
abc def <gibmireinbier@gmail.com>
2019-05-17 11:47:49 +0700

    update last stable code

--- src/service/contracts/pairExService.js
+++ src/service/contracts/pairExService.js
@@ -4,7 +4,8 @@ import { empty } from 'glamor';
 import { cutString, weiToMNTY, weiToEthS} from './help'
 import { DECIMALS } from '@/constant'
 
-var BigNumber = require('big-number');
+// var BigNumber = require('big-number');
+var BigNumber = require('bignumber.js');
 
 export default class extends BaseService {
     async sellVolatileToken(haveAmount, wantAmount) {
@@ -21,14 +22,18 @@ export default class extends BaseService {
         let methods = store.contracts.pairEx.methods
         let res = await methods.getOrder(_orderType, _id).call()
         let weiMNTY = _orderType ? BigNumber(await res[2]) : BigNumber(await res[1])
+        weiMNTY = weiMNTY.toFixed(0)
+        // console.log('weiMNTY', weiMNTY)
         let weiNUSD = _orderType ? BigNumber(await res[1]) : BigNumber(await res[2])
+        weiNUSD = weiNUSD.toFixed(0)
         let amount = weiToMNTY(await weiMNTY)
         // let price = NUSDs / 1 MNTY = (weiNUSD / 1e18) / (weiMNTY / 1e24) = 1e6 * weiNUSD / weiMNTY
-        let wPrice = BigNumber(await weiNUSD).multiply(BigNumber(10).power(DECIMALS.mnty)).div(await weiMNTY) // weiNUSD / 1 MNTY
-        let expo = BigNumber(10).power(DECIMALS.nusd)
-        let _before = BigNumber(wPrice).div(expo)
-        let _after = BigNumber(wPrice).mod(expo)
-        let price = _before.toString() + '.' + _after.toString()
+        let wPrice = BigNumber(await weiNUSD).multipliedBy(BigNumber(10).pow(DECIMALS.mnty)).div(await weiMNTY).div(BigNumber(10).pow(DECIMALS.nusd)) // weiNUSD / 1 MNTY
+        // let expo = BigNumber(10).pow(DECIMALS.nusd)
+        // let _before = BigNumber(wPrice).div(expo)
+        // let _after = BigNumber(wPrice).mod(expo)
+        // let price = _before.toString() + '.' + _after.toString()
+        let price = wPrice.toFixed(10)
         let order = await {
             'id': _id,
             'maker': cutString(res[0]),
@@ -59,6 +64,8 @@ export default class extends BaseService {
             await loop--
         }
         await console.log('ABCD', orders)
+        let orderNull = await this.getOrder(_orderType, byteZero)
+        await console.log('order Zero', orderNull)
         //orders = await orders.sort((a, b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0))
         if (_orderType) {
             await this.dispatch(pairExRedux.actions.orders_update({'true': orders.reverse()}))
