d9a04cb524075616f29400770a96626fa6aea1c7
Zergity <zergity@gmail.com>
2019-11-22 14:58:14 +0700

    only create web3 instance when the default account is changed

--- src/app.js
+++ src/app.js
@@ -67,19 +67,20 @@ function setupWeb3 () {
             console.error(err);
             return;
           }
-          const web3 = new Web3z(window.ethereum)
-
-          const contracts = {
-            VolatileToken: new web3.eth.Contract(CONTRACTS.VolatileToken.abi, CONTRACTS.VolatileToken.address),
-            StableToken: new web3.eth.Contract(CONTRACTS.StableToken.abi, CONTRACTS.StableToken.address),
-            Seigniorage: new web3.eth.Contract(CONTRACTS.Seigniorage.abi, CONTRACTS.Seigniorage.address),
-          }
 
           // detect account switch
           const wallet = store.getState().user.wallet;
           isLoggedIn = isLoggedIn && wallet === accounts[0];
 
           if (!isLoggedIn) {
+            const web3 = new Web3z(window.ethereum)
+
+            const contracts = {
+              VolatileToken: new web3.eth.Contract(CONTRACTS.VolatileToken.abi, CONTRACTS.VolatileToken.address),
+              StableToken: new web3.eth.Contract(CONTRACTS.StableToken.abi, CONTRACTS.StableToken.address),
+              Seigniorage: new web3.eth.Contract(CONTRACTS.Seigniorage.abi, CONTRACTS.Seigniorage.address),
+            }
+
             store.dispatch(userRedux.actions.loginMetamask_update(true))
             store.dispatch(contractsRedux.actions.volatileToken_update(contracts.VolatileToken))
             store.dispatch(contractsRedux.actions.stableToken_update(contracts.StableToken))
